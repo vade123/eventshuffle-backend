a1bec0a1a703a5f0279ea5cae183ee0def456762
Valtteri Valtanen <valtteri.valtanen@tuni.fi>
2019-12-29 15:14:03 +0200

    added method POST for new events

--- controllers/events.js
+++ controllers/events.js
@@ -1,3 +1,17 @@
 const eventRouter = require('express').Router();
+const Event = require('../models/event');
+
+eventRouter.post('/', (req, res) => {
+  const event = new Event({
+    name: req.body.name,
+    dates: req.body.dates,
+    votes: [],
+  });
+  event
+    .save()
+    .then((result) => {
+      res.status(201).json({ id: result.id });
+    });
+});
 
 module.exports = eventRouter;
